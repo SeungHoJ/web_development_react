168e7f90ea3fc5f89b712405c840817626792a4e
seungho <tkdgus7993@naver.com>
2025-02-25 21:46:00 +0900

    feat:20250225 React lesson started

---
+++ vite-project/src/example.js
@@ -0,0 +1,59 @@
+// JavaScript 복습 및 React 적용 문법 #1
+let a = 1;
+console.log(a);
+
+// 지역변수 / 전역 변수 개념
+let count = 10;
+if(count > 5){
+  const total = count * 2;
+  console.log(total);
+}
+
+// console.log(total); // {} 범위를 벗어났기 때문에 오류가 발생함
+
+// const가 객체 / 배열
+
+const myObj = {foo:3};
+myObj.foo = 5;
+console.log(myObj.foo);
+
+/**
+ * const가 상수이긴는ㄴ ㅏㄴ데, 객체 또는 배열인 경우 내부의 송성이나 요소를 업데이트 할 수 있습니다
+ * 
+ * 
+ */
+
+// 화살표 함수
+
+// JS 상에서 함수를 정의하는 방법 #1
+function multiply(x){
+  return x*2;
+}
+
+console.log(multiply(2))
+// 이상의 코드를 화살표 함수를 적용했을 때는
+x => x*2;
+
+const calc = x => x*2;
+console.log(calc(3));
+
+// 매개변수가가 두 개 이상인 경우는 () 로 묶어줘야 합니다
+
+const calcSum = (x,y) => x+y;
+console.log(calcSum(1,2));
+
+// 함수 본문이 return 값인 경우 명시할 필요가 없습니다(이상에서 볼수 있듯이), 그런데 함수 본문{}이 여러줄에 걸칠 경우에는 중괄호는 명시해줘야만 합니다
+
+const calcMulti = (x,y) => {
+  console.log('곱셈결과는 다음과 같습니다.')
+  return x*y
+}
+
+console.log(calcMulti(5,6));
+
+// 함수에 파라미터가 없다면 빈 괄호로 작성해야만 합니다
+
+const sayHello = () => console.log("안녕하세요")
+
+sayHello();
+
---
+++ vite-project/src/example2.js
@@ -0,0 +1,31 @@
+// // JavaScript 복습 및 React 적용 문법 #2
+
+// // 템플릿 리터럴
+// let person = {firstName:'John', lastName: 'Johnson'};
+// let greeting = "Hello " + person.firstName + " " + person.lastName;
+// console.log(greeting);
+
+// // greeting2를 템플릿 리터럴을 적용하여 작성하고 consle창에 띄우시오.
+// // 백틱 사용하셔야 합니다
+
+// let greeting2 = `Hello ${person.firstName} ${person.lastName}`
+// console.log(greeting2);
+
+// // 객체 구조 분해
+// // 해당 기능을 사용하면 객체에 값을 추출하여 변수에 할당할 수 있음.
+// // 단일 구문을 이용하면 객체의 여러 속성을 개별 변수에 할당하는 것도 가능하기 때문에 학습합니다
+
+// const person2 ={
+//   firstName : 'Jane',
+//   lastName : 'Cooper',
+//   email : 'j.cooper@mail.com'
+// }
+
+// const firstName1 = person.firstName;
+// const lastName - person.lastName;
+// const email1 = person.email;
+
+// const {firstName, lastName, email} = person2;
+
+// console.log()
+
