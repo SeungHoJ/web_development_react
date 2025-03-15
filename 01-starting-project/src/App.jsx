ed11d1579262a85128a60f9dd214ea32ebe8bb45
seungho <tkdgus7993@naver.com>
2025-03-15 15:54:14 +0900

    feat: 20250312 react page - props

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,12 +1,28 @@
-import Header from "./components/Header";
+import Header from "./components/Header/Header2.jsx";
+import  { CORE_CONCEPTS } from './data.js'
 
+function CoreConcepts(props){
+  return <li>
+    <img src={props.image} alt={props.title} />
+    <h3>{props.title}</h3>
+    <p>{props.description}</p>
+  </li>
+}
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
-        <h2>시작 해봅시다 !!</h2>
+        
+        <section id="core-concepts">
+        <h2>핵심 개념들 of React</h2>
+        <ul>
+          <CoreConcepts image={CORE_CONCEPTS[0].image} title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description}/>
+          <CoreConcepts image={CORE_CONCEPTS[1].image} title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description}/>
+          <CoreConcepts image={CORE_CONCEPTS[2].image} title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description}/>
+          <CoreConcepts image={CORE_CONCEPTS[3].image} title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description}/>
+        </ul>
+        </section>
       </main>
     </div>
   );
--- 01-starting-project/src/components/Header.jsx
+++ 01-starting-project/src/components/Header.jsx
@@ -1,25 +1,25 @@
-import reactImg from "../assets/react-core-concepts.png"
-const reactDescriptionsKor = ['근본적인', '결정적인','핵심적인'];
-const reactDescriptionsEng = ['Fundamental', 'Crucial','Core','Hello']
+// import reactImg from "../assets/react-core-concepts.png"
+// const reactDescriptionsKor = ['근본적인', '결정적인','핵심적인'];
+// const reactDescriptionsEng = ['Fundamental', 'Crucial','Core','Hello']
 
-function getRandomInt(max){
-  return Math.floor(Math.random() *  (max + 1));
-}
+// function getRandomInt(max){
+//   return Math.floor(Math.random() *  (max + 1));
+// }
 
-function Header(){
-  const num = reactDescriptionsEng.length-1;
-  return(
-    <header>
-        <img src={reactImg} alt="Stylized atom" />
-        <h1>React 필수 요소 복습</h1>
-        <p>
-          {reactDescriptionsEng[getRandomInt(num)]} React concepts you will need for almost any app you are
-          going to build!
-        <br />
-        대부분의 앱을 위해 당신이 필요로 할 근복적이 React 개념 학습
-        </p>
-      </header>
-  )
-}
+// function Header(){
+//   const num = reactDescriptionsEng.length-1;
+//   return(
+//     <header>
+//         <img src={reactImg} alt="Stylized atom" />
+//         <h1>React 필수 요소 복습</h1>
+//         <p>
+//           {reactDescriptionsEng[getRandomInt(num)]} React concepts you will need for almost any app you are
+//           going to build!
+//         <br />
+//         대부분의 앱을 위해 당신이 필요로 할 근복적이 React 개념 학습
+//         </p>
+//       </header>
+//   )
+// }
 
-export default Header;
+// export default Header;
