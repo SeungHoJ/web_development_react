e98944baccd440e31ac03c16c1c3300e4144ad69
seungho <tkdgus7993@naver.com>
2025-03-25 18:56:18 +0900

    feat: 20250321 reacttodoapp started

--- usememoapp/src/App.jsx
+++ usememoapp/src/App.jsx
@@ -1,47 +1,33 @@
-import { useMemo, useState } from 'react'
+import { useMemo, useState,useEffect } from 'react'
 
 import './App.css'
-import ShowState from './components/ShowState';
 
 function App() {
-  const [ number, setNumber] = useState(0);
-  const [ text, setText] = useState('');
+  const [ number,setNumber] = useState(0);
+  const [ isTrue,setIsTrue] = useState(true);
 
-  function heavyCalc(){
-    console.log('반복수행중')
-    let sum = 0;
-    for(let i = 0; i < 1000000; i++){
-      sum += i;
-    }
-    return sum;
-  }
-  let calc = useMemo(() => {
-    return heavyCalc();
-  },[])
+  // const answer = isTrue ? "true" : "false"
 
-  const increaseNum = () => {
-    setNumber((prev) => prev + 1)
-  }
+  // const answer = {bool:isTrue ? "true" : "false" }
+  // 객체 주소값이 렌더링 될 때 마다 리렌더링이 발생
+  const answer = useMemo(() => {
+    return { bool : isTrue ? "true" : "false"}
+  },[isTrue])
 
-  const decreaseNum = () => {
-    setNumber((prev) => prev - 1)
-  }
 
-  const handleText = e => {
-    setText(e.target.value)
-  }
+  useEffect(() => {
+    console.log('answer값이 변경되었습니다')
+  },[answer])
+ 
   return (
-    <>
-      <h4>반복적이고 무거운 연산값</h4>
-      <p>{calc}</p>
-      <br /><br />
-      <h4>숫자 바꾸기</h4>
-      <button onClick={increaseNum}>+</button> <button onClick={decreaseNum}>-</button>
-      <h4>문자 바꾸기</h4>
-      <input type="text" onChange={handleText} />
-      <br /><br />
-      <ShowState text={text} number={number}/>
-    </>
+    <div>
+      <p>number</p>
+      <input type="number" value={number} onChange={(e) => setNumber(e.target.value)} />
+      <hr />
+      <p>answer</p>
+      <p>True or False ? : {answer.bool}</p>
+      <button onClick={e => setIsTrue(!isTrue)}>Reverse</button>
+    </div>
   )
 }
 
