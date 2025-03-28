d25ae578093dca8bd8bff98db7ea082bf2d50e91
seungho <tkdgus7993@naver.com>
2025-03-28 22:58:46 +0900

    feat: 20250324 movie app component 분리

--- movieapp/src/App.jsx
+++ movieapp/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch ,Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 import './App.css'
 import Home from './routes/Home';
 import Detail from './routes/Detail';
@@ -6,16 +6,12 @@ import Detail from './routes/Detail';
 function App() {
 
   return (
-    <Router>
-      <Switch>
-        <Route path='/movie/:id'>
-          <Detail />
-        </Route>
-        <Route path='/'>
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+   <Router>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/movie/:id' element={<Detail />} />
+    </Routes>
+   </Router>
   )
 }
 
